9e70cfa391a1fc4363d80c957b0e93180b658989
Edgar Veress <veress.edgar@yahoo.com>
2022-09-21 20:38:43 +0100

    Early triage

---
+++ script.js
@@ -0,0 +1,16 @@
+const gameBoardObj = () => {
+    const gameBoardArr = []
+}
+
+const player = () => {
+    
+}
+
+const gameFlow = () => {
+
+}
+
+let test = document.querySelector('td')
+document.querySelector('button').addEventListener('click', () => {
+    test.classList.add('test');
+})
