e7ec5332ae1ebf5993a1e2e7a693c8d29e6987b4
EdgarV-95 <edgar.veress@yahoo.com>
2022-09-21 22:05:38 +0100

    Started controlFlow()

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const gameBoardObj = () => {
-    const gameBoardArr = []
+// Create object with 2 functions one for X and one for O
+let allTds = document.querySelectorAll('td')
+
+const controlFlow = () => {
+    const move = (option) => {
+        allTds.forEach(el => {
+            el.addEventListener('click', () => {
+                // el.innerHTML = '<p>X</p>'
+                el.innerHTML = option
+            })
+        })
+    }
+    return {move}
 }
 
-const player = () => {
-    
-}
-
-const gameFlow = () => {
-
-}
-
-let test = document.querySelector('td')
 document.querySelector('button').addEventListener('click', () => {
-    test.classList.add('test');
+    allTds.forEach(el => {
+        el.innerHTML = ''
+    })
 })
+
+let test1 = controlFlow()
+moveX = test1.move('<p>X</p>')
+moveO = test1.move('<p>O</p>')
+console.log(moveX)
+console.log(moveO)
+
+const orderArr = [moveX, moveO, moveX, moveO, moveX, moveO, moveX, moveO, moveX]
+orderArr.forEach(el => el)
